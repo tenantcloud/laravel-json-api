acd53fa18b8139aa909a976b6a79637d67caa3fd
Taras Chuchvara <taras@chuchvara.co.ua>
2023-07-27 10:42:11 +0300

    doc: Removed doc block

--- src/JsonApiSerializer.php
+++ src/JsonApiSerializer.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the League\Fractal package.
- *
- * (c) Phil Sturgeon <me@philsturgeon.uk>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace TenantCloud\JsonApi;
 
 use League\Fractal\Resource\ResourceInterface;
