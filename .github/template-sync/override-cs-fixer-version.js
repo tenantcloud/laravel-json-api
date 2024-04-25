25bccfe23eb51e5d4438b2862b263f25ae20dbef
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2024-04-25 15:35:32 +0300

    fix: Failing code style

---
+++ .github/template-sync/override-cs-fixer-version.js
@@ -0,0 +1,22 @@
+const { readFile, writeFile } = require('fs/promises');
+
+module.exports = () => async (template, source) => {
+	const sourceComposerJson = JSON.parse(
+		(await readFile(source.path('composer.json'))).toString(),
+	);
+
+	await writeFile(
+		source.path('composer.json'),
+		JSON.stringify({
+			...sourceComposerJson,
+			'require-dev': {
+				...sourceComposerJson['require-dev'],
+				'php-cs-fixer/shim': '3.19.1',
+			},
+		}),
+	);
+
+	return {
+		reserved: [],
+	};
+};
